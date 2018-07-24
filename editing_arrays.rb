d2c2d92a7656293c500e67055d7e73adaa1d1ce8
Marnie Reid <marniejeanreid@gmail.com>
2018-07-24 14:07:32 +0000

    Done.

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,6 +6,9 @@ rainbow_colors = [
 
 def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  puts rainbow_colors[0]= "red"
+  puts rainbow_colors[1]= "light_red"
+  puts rainbow_colors[0]= "light_yellow"
 end
 
 def add_colors
